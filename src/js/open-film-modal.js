e7ead65921822a27ea882ddfd83777d8b8216078
Alexander Khardin <khporter1984@gmail.com>
2022-09-10 22:40:56 +0300

    resolve conflicts

--- src/js/open-film-modal.js
+++ src/js/open-film-modal.js
@@ -21,8 +21,6 @@ function onGalleryBoxClick(event) {
   }
   const filmId = event.target.closest('.card').id;
 
-  console.log('film id: ', filmId);
-
   fetchFilmDetailsById(filmId)
     .then(filmDetails => {
       clearFilmModalMarkup();
