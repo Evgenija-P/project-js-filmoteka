269c39d1482a7639e1e61dd991eb4415ee96abd4
Evgenija-P <e.a.poduzova@gmail.com>
2022-09-11 13:04:45 +0300

    up+down+modal

---
+++ src/js/button-up-down.js
@@ -0,0 +1,20 @@
+jQuery(function () {
+  if ($(window).scrollTop() >= '250') $('#ToTop').fadeIn('slow');
+  $(window).scroll(function () {
+    if ($(window).scrollTop() <= '250') $('#ToTop').fadeOut('slow');
+    else $('#ToTop').fadeIn('slow');
+  });
+  if ($(window).scrollTop() <= $(document).height() - '999')
+    $('#OnBottom').fadeIn('slow');
+  $(window).scroll(function () {
+    if ($(window).scrollTop() >= $(document).height() - '999')
+      $('#OnBottom').fadeOut('slow');
+    else $('#OnBottom').fadeIn('slow');
+  });
+  $('#ToTop').click(function () {
+    $('html,body').animate({ scrollTop: 0 }, 'slow');
+  });
+  $('#OnBottom').click(function () {
+    $('html,body').animate({ scrollTop: $(document).height() }, 'slow');
+  });
+});
