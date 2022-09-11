dd9956f629df707b3c871381d26316c2efe06da4
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2022-09-11 19:09:22 +0000

    Deploying to gh-pages from @ Evgenija-P/project-js-filmoteka@36373ee127e6af1c996c1e282553c37727bda395 🚀

--- my-library.16f18d38.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={watchedBtn:document.querySelector(".watched-btn"),queueBtn:document.querySelector(".queue-btn")};e.watchedBtn.addEventListener("click",(function(){e.watchedBtn.classList.add("active"),e.queueBtn.classList.remove("active")})),e.queueBtn.addEventListener("click",(function(){e.queueBtn.classList.add("active"),e.watchedBtn.classList.remove("active")}))}();
-//# sourceMappingURL=my-library.16f18d38.js.map
