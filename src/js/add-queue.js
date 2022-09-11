4f2193be9c96c6bb432e05b45f07eef79f112e9b
webnovelty <webnoveltystudio@gmail.com>
2022-09-11 22:42:13 +0200

    fixbuttns

--- src/js/add-queue.js
+++ src/js/add-queue.js
@@ -1,29 +1,26 @@
 import uniqBy from 'lodash.uniqby';
-// import { egg } from './open-film-modal.js';
 
-// const buttonEL = document.querySelector('[button-add-queue]');
 
 let queueFilms = [];
 let uniqQueueFilms = [];
 
 export const dataSaveQueue = function (data) {
-  queueFilms.push(data);
+	queueFilms.push(data);
 
-  uniqQueueFilms = uniqBy(queueFilms, 'id');
+	uniqQueueFilms = uniqBy(queueFilms, 'id');
 
-  localStorage.setItem('queue', JSON.stringify(uniqQueueFilms));
+	localStorage.setItem('queue', JSON.stringify(uniqQueueFilms));
+
+	const storedFilms = JSON.parse(localStorage.watched);
+
+	for (var i = 0; i < storedFilms.length; i++) {
+		if (storedFilms[i].id === data.id) {
+			storedFilms.splice(i, 1);
+		}
+	}
+	localStorage.watched = JSON.stringify(storedFilms);
 };
 
 if (localStorage.getItem('queue')) {
-  queueFilms = JSON.parse(localStorage.getItem('queue'));
+	queueFilms = JSON.parse(localStorage.getItem('queue'));
 }
-
-if (localStorage.getItem('queue')) {
-  queueFilms = JSON.parse(localStorage.getItem('queue'));
-}
-
-// buttonEL.addEventListener('click', onClickAddWatch);
-// async function onClickAddWatch(e) {
-// 	e.preventDefault();
-// 	dataSaveQueue(egg);
-// }
