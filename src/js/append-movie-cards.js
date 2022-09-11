b20c284e133c28dc31b7cfbfbb8ba6c3f472962d
Anton-Starshinov <97900625+Anton-Starshinov@users.noreply.github.com>
2022-09-11 17:14:11 +0300

    add spiner

--- src/js/append-movie-cards.js
+++ src/js/append-movie-cards.js
@@ -1,5 +1,7 @@
 import NewApi from './fetch-movies-homepg';
 import { markupMovies } from './markup-movie-card';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+
 const newApi = new NewApi();
 const refs = {
   gallery: document.querySelector('#homepage__gallery'),
@@ -7,8 +9,14 @@ const refs = {
 
 export async function appendMarkupMovies() {
   try {
+    Loading.dots({
+      svgColor: 'red',
+    });
+
     const { data } = await newApi.fetchMovies();
     refs.gallery.insertAdjacentHTML('beforeend', markupMovies(data.results));
+
+    Loading.remove();
   } catch (error) {
     console.log(error.message);
   }
