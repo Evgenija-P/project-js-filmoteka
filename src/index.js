59cffe3eb4e41a0eb42b5938824a22387354e3c6
Evgenija-P <e.a.poduzova@gmail.com>
2022-09-11 22:31:34 +0300

    js v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './js/add-queue';
 import './js/add-watch';
 import './js/load-more_btn';
+import './js/button-up-down';
+import './js/group-modal';
+import './js/append-movie-cards';
+import './js/open-film-modal';
+import './js/film-modal';
