3c0b4493509018709c0cb61050b0a5b59d71ebf7
webnovelty <webnoveltystudio@gmail.com>
2022-09-12 21:13:37 +0200

    fullpls

--- src/js/add-queue.js
+++ src/js/add-queue.js
@@ -1,26 +1,28 @@
 import uniqBy from 'lodash.uniqby';
+import { watchFilms } from './add-watch';
 
 
-let queueFilms = [];
+export let queueFilms = [];
 let uniqQueueFilms = [];
 
+
 export const dataSaveQueue = function (data) {
+
+	if (localStorage.watched) {
+		const storedFilms = JSON.parse(localStorage.watched);
+
+		for (var i = 0; i < storedFilms.length; i++) {
+			if (storedFilms[i].id === data.id) {
+				storedFilms.splice(i, 1);
+				watchFilms = [];
+			}
+		}
+		localStorage.watched = JSON.stringify(storedFilms);
+	}
+
 	queueFilms.push(data);
-
 	uniqQueueFilms = uniqBy(queueFilms, 'id');
-
 	localStorage.setItem('queue', JSON.stringify(uniqQueueFilms));
 
-	const storedFilms = JSON.parse(localStorage.watched);
 
-	for (var i = 0; i < storedFilms.length; i++) {
-		if (storedFilms[i].id === data.id) {
-			storedFilms.splice(i, 1);
-		}
-	}
-	localStorage.watched = JSON.stringify(storedFilms);
 };
-
-if (localStorage.getItem('queue')) {
-	queueFilms = JSON.parse(localStorage.getItem('queue'));
-}
